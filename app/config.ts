f9c2fbc8d38efc15e078eed951609329f9a9aaf1
Jihyuk Lee <jihyuk1714@gmail.com>
2024-10-02 16:26:57 +0900

    fix :: 저작권 표기 재수정

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   title: 'test',
-  licenceTitle: 'test',
+  licenceTitle: 'fodog',
   bio: '블로그 테스트 중이에요',
   githubLink: 'https://github.com/withJihyuk',
   xLink: 'https://x.com/enbraining',
