d87773453503073b08d038c23743ec80b1cf7449
Jihyuk Lee <jihyuk1714@gmail.com>
2024-10-02 16:38:09 +0900

    feat :: 사이트명 너무 길어요..

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  title: '<b>withJihyuk develop<b/>',
+  title: '<b>JihyukLab<b/>',
   licenceTitle: 'fodog',
   bio: '<b>Flutter Engineer</b>를 전공으로, 다양한 활동들을 해보고 있어요. <br/><a href="lab.mya.ong">홈서버</a>도 구성하고 있답니다.',
   githubLink: 'https://github.com/withJihyuk',
