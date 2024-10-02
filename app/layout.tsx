d4c2dbdb187e718d2be6949a85747fca44fb90fb
Jihyuk Lee <jihyuk1714@gmail.com>
2024-10-02 16:34:54 +0900

    feat :: 명칭 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = LocalFont({ src: '../public/fonts/PretendardVariable.woff2' });
 
 export const metadata: Metadata = {
-  title: 'withJihyuk',
+  title: 'withJihyuk develop',
   description: '플러터 엔지니어의 블로그입니다.',
 };
 
