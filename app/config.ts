5d34bb8c106fa230052acf90a4eab8ca46cf77ce
Jihyuk Lee <jihyuk1714@gmail.com>
2024-10-02 16:45:13 +0900

    fix :: a href엔 자동 포인터라 빼기

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   title: '<b>JihyukLab<b/>',
   licenceTitle: 'fodog',
-  bio: '<b>Flutter Engineer</b>를 전공으로, 다양한 활동들을 해보고 있어요. <br/><a className="cursor-pointer font-bold" href="lab.mya.ong">홈서버</a>도 구성하고 있답니다.',
+  bio: '<b>Flutter Engineer</b>를 전공으로, 다양한 활동들을 해보고 있어요. <br/><a className="font-bold" href="lab.mya.ong">홈서버</a>도 구성하고 있답니다.',
   githubLink: 'https://github.com/withJihyuk',
   xLink: '',
 };
